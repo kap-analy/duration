6a535af9baf1a4b74c1ebfbc640eea98a72948d4
cindyyeon <cindy_yeon@naver.com>
2021-04-02 15:36:29 +0900

    전 채권에 대해 실행되게 함

--- bond.py
+++ bond.py
@@ -13,15 +13,15 @@ from dateutil.relativedelta import relativedelta
 class Bond:
 
     # 채권 정보: 발행일, 만기일, 쿠폰, 이자계산지급월수
-
-    def __init__(self, td, issue_date, due_date, amount = 10000, coupon_rate, interestpaycalmcnt):
+    def __init__(self, td, issue_date, due_date, amount,
+                 coupon_rate, num_of_interest_payment, interest_payment_type):
         self.td = td
         self.issue_date = issue_date
         self.due_date = due_date
         self.amount = amount
         self.coupon_rate = coupon_rate
-        self.interestpaycalmcnt = interestpaycalmcnt
-
+        self.num_of_interest_payment = num_of_interest_payment
+        self.interest_payment_type = interest_payment_type
     # 채권 현금흐름을 계산해 주는 함수입니다.
     # 위 채권정보가 입력되고, [cash_time, cash_amt] 정보가 있는 pandas dataframe을 출력합니다.
 
@@ -29,27 +29,89 @@ class Bond:
         # 날짜를 datetime 타입으로 변경합니다.
         issue_date = datetime.strptime(self.issue_date, '%Y%m%d').date()
         due_date = datetime.strptime(self.due_date, '%Y%m%d').date()
+        maturity = math.ceil((due_date - issue_date).days / 365)
         td = datetime.strptime(self.td, '%Y%m%d').date()
-        diff = (due_date - issue_date).days / 365 # 날짜 차이
-        cf_num = math.ceil(diff * 12 / self.interestpaycalmcnt)
-
-        cf = []
-
-        for i in range(cf_num):
-            if i == 0:
-                cf_td = issue_date
-            if i == cf_num:
-                cf_td = due_date
-                cash_amt = self.amount + self.amount * self.coupon_rate * self.interestpaycalmcnt / 12
-            else:
-                cf_td += relativedelta(months=self.interestpaycalmcnt)
-                cash_amt = self.amount * self.coupon_rate * self.interestpaycalmcnt / 12
+        # 이자지급구분에 따라 계산식을 달리합니다.
+        cf_td_data = []
+        if self.interest_payment_type == '11': # 할인채인 경우
+            cf_td = due_date
             cash_time = (cf_td - td).days / 365
-            cf.append([cf_td, cash_time, cash_amt])
-        cf = pd.DataFrame(cf, columns=['cf_td', 'cash_time', 'cash_amt'])
-        cf.drop(['cf_td'], axis=1, inplace=True)
-        # cf는 pandas dataframe 입니다.
+            cash_amt = self.amount
+            cf_td_data.append([cf_td, cash_time, cash_amt])
+
+        elif self.interest_payment_type == '12': # 복리채인 경우
+            cf_td = due_date
+            cash_time = (cf_td - td).days / 365
+            cash_amt = self.amount*((1+self.coupon_rate/self.num_of_interest_payment)**(self.num_of_interest_payment*maturity))
+            cf_td_data.append([cf_td, cash_time, cash_amt])
+
+        elif self.interest_payment_type == '14': # 단리채인 경우
+            cf_td = due_date
+            cash_time = (cf_td - td).days / 365
+            cash_amt = self.amount * (1 + self.coupon_rate * maturity)
+            cf_td_data.append([cf_td, cash_time, cash_amt])
+
+        elif self.interest_payment_type == '15': #복5단2인 경우
+            cf_td = due_date
+            cash_time = (cf_td - td).days / 365
+            cash_amt = self.amount * ((1 + self.coupon_rate / self.num_of_interest_payment) ** (self.num_of_interest_payment * 5))
+            cash_amt += self.amount * self.coupon_rate * 2
+            cf_td_data.append([cf_td, cash_time, cash_amt])
+
+        else:  # 이표채인 경우
+            diff = (due_date - issue_date).days / 365 # 날짜 차이
+            cf_num = math.ceil(diff * self.num_of_interest_payment)
+            interest_amount = self.amount * self.coupon_rate * 1 / self.num_of_interest_payment
+
+            cf_td = issue_date
+            # 발행일부터 만기일까지의 현금흐름 발생일을 만들어 낸다.
+            i = 1
+            while i <= cf_num:
+                cf_td += relativedelta(months=12/self.num_of_interest_payment)
+                if (cf_td - td).days > 0:
+                    cash_amt = interest_amount
+                    if i == cf_num:  # 마지막 현금흐름의 경우
+                        if cf_td == due_date:
+                            cash_amt = self.amount + interest_amount
+                        else:
+                            cf_td = due_date
+                            cash_amt = self.amount
+                            if len(cf_td_data) == 0:
+                                cash_amt += interest_amount * ((due_date - td).days / 365) * self.num_of_interest_payment
+                            else:
+                                cash_amt += interest_amount * ((due_date - prev_cf_td).days / 365) * self.num_of_interest_payment
+                    cash_time = (cf_td - td).days / 365
+                    cf_td_data.append([cf_td, cash_time, cash_amt])
+                    prev_cf_td = cf_td
+                i += 1
+        cf = pd.DataFrame(cf_td_data, columns=['cf_td', 'cash_time', 'cash_amt'])
+        # cf.drop(['cf_td'], axis=1, inplace=True)
         return cf
+        # for i in range(cf_num):
+        #     if i == 0:
+        #         cf_td = issue_date
+        #     elif i == (cf_num - 1):
+        #         cf_td = due_date
+        #         cash_amt = self.amount
+        #         # 잔존만기까지의 남은 기간만큼의 이자를 계산해서 더해 줍니다
+        #         # 원래 이자금액 = (self.amount * self.coupon_rate * self.interestpaycalmcnt / 12)
+        #         # 잔존만기까지의 남은 기간 (cf_td - cf[i-1][0]).days
+        #         chk_td = lambda x: x if x in locals() else td
+        #         cash_amt += (self.amount * self.coupon_rate * self.interestpaycalmcnt / 12) * ((cf_td - chk_td(prev_cf_td)).days / 365)/(self.interestpaycalmcnt/12)
+        #     else:
+        #         cf_td += relativedelta(months=self.interestpaycalmcnt)
+        #         cash_amt = self.amount * self.coupon_rate * self.interestpaycalmcnt / 12
+        #     # 듀레이션 산출 시점 기준으로 예전 현금흐름이면 버린다.
+        #     if (cf_td - td).days < 0:
+        #         next
+        #     else:
+        #         cash_time = (cf_td - td).days / 365
+        #         cf.append([cf_td, cash_time, cash_amt])
+        #         # 현금흐름이 발생했던 날을 저장해 둔다. (잔존만기까지의 이자 계산에 필요)
+        #         prev_cf_td = cf_td
+
+        # cf는 pandas dataframe 입니다.
+
     # 채권 현금흐름이 발생하는 시점별로 spot curve 데이터 상에서 어디에 위치하는지 찾아본다
     # 예) 4개월 후의 현금흐름은 spot curve 데이터 상에서 3개월과 6개월 사이에 위치한다
 
@@ -64,31 +126,33 @@ class Bond:
             chk_data = spot_yield_data.iloc[:]['key_rate'].values
             cash_idx = search_number_in_array(chk_value, chk_data)
             # search_number가 array 데이터 중 숫자 크기상으로 어디에 위치하는지를 반환합니다.
-            #         if cash_idx == 0:
-            #             apply_yield.append(spot_yield_data.iloc[i]['yield'].values)
-
-            #         elif cash_idx == len(spot_yield_data) - 1 :
-            #             apply_yield.append(spot_yield_data.iloc[len(spot_yield_data)]['yield'].values)
-
-            #         else:
-            data_diff = spot_yield_data.iloc[cash_idx] - spot_yield_data.iloc[cash_idx - 1]
-            apply_yield_value = spot_yield_data.iloc[cash_idx - 1]['yield']
-            apply_yield_value += data_diff['yield'] / data_diff['key_rate'] * (
-                        cf_data.iloc[i]['cash_time'] - spot_yield_data.iloc[cash_idx - 1]['key_rate'])
+            if cash_idx == 0:
+                # 다음 현금흐름 발생시점이 3개월 미만인 경우 3M spot yield를 쓴다
+                apply_yield_value = spot_yield_data.iloc[0]['yield']
+            elif cash_idx == len(spot_yield_data):
+                # 현금흐름 발생시점이 30년을 초과하는 경우 30Y의 spot yield를 쓴다.
+                apply_yield_value = spot_yield_data.iloc[len(spot_yield_data)-1]['yield']
+            else:
+                data_diff = spot_yield_data.iloc[cash_idx] - spot_yield_data.iloc[cash_idx - 1]
+                apply_yield_value = spot_yield_data.iloc[cash_idx - 1]['yield']
+                apply_yield_value += data_diff['yield'] / data_diff['key_rate'] * (cf_data.iloc[i]['cash_time'] - spot_yield_data.iloc[cash_idx - 1]['key_rate'])
             apply_yield.append(apply_yield_value)
 
         cf_data['apply_yield'] = apply_yield
-        cf_data['pv_factor'] = 1 / (1 + cf_data['apply_yield'] / 100 / (12 / interestpaycalmcnt)) ** (cf_data['num'])
-        cf_data['pv_cash_amt'] = cf_data['pv_factor'] * cf_data['cash_amt']
+        if self.interest_payment_type == '11': # 할인채의 경우
+            bond_price = self.amount / ((1 + cf_data['apply_yield'] /100) ** cf_data['cash_time'])
+        else:
+            cf_data['pv_factor'] = 1 / (1 + cf_data['apply_yield'] / 100 / self.num_of_interest_payment) ** (cf_data.index + 1)
+            cf_data['pv_cash_amt'] = cf_data['pv_factor'] * cf_data['cash_amt']
+            bond_price = cf_data['pv_cash_amt'].sum()
 
-        bond_price = cf_data['pv_cash_amt'].sum()
         return bond_price
 
-    def key_rate_duration(self, spot_yield_data, delta=0.01):
+    def key_rate_duration(self, cf_data, spot_yield_data, delta=0.01):
 
         # delta는 금리변동분입니다. % 단위로 씁니다. 1bp 는 0.01 입니다.
 
-        price = self.price(spot_yield_data)
+        price = self.price(cf_data, spot_yield_data)
         key_rate_duration = []
         for i in range(len(spot_yield_data)):
             spot_curve_plus = spot_yield_data.copy()
@@ -97,8 +161,8 @@ class Bond:
             spot_curve_plus.iloc[i]['yield'] += delta
             spot_curve_minus.iloc[i]['yield'] -= delta
 
-            price_plus = self.price(spot_curve_plus)
-            price_minus = self.price(spot_curve_minus)
+            price_plus = self.price(cf_data,  spot_curve_plus)
+            price_minus = self.price(cf_data, spot_curve_minus)
             duration = (price_minus - price_plus) / (2 * price * delta / 100)
             key_rate_duration.append(duration)
 
--- krd.py
+++ krd.py
@@ -18,7 +18,9 @@
 from bond import *
 from get_data import *
 from logging_pack import *
+import timeit
 
+start_time = timeit.default_timer() #시작 시간 체크
 
 # 샘플데이터 생성
 # cash_count: 현금흐름의 갯수
@@ -31,32 +33,91 @@ from logging_pack import *
 # 데이터 입력 부분                                                       #
 ########################################################################
 
-cash_count = 5
-coupon_rate = 0.03
-amount = 10000
-interestpaycalmcnt = 6
-maturity = 3
+# cash_count = 5
+# coupon_rate = 0.03
+# amount = 10000
+# interestpaycalmcnt = 6
+# maturity = 3
 
-cf_raw = [[1, 0.533, 2500000], [2, 1.033, 2500000], [3, 1.533, 2500000], [4, 2.033, 2500000], [5, 2.533, 2500000],
-          [6, 3.033, 2500000], [7, 3.533, 2500000], [8, 4.033, 2500000], [9, 4.533, 2500000], [10, 5.033, 2500000],
-          [11, 5.533, 2500000], [12, 6.033, 2500000], [13, 6.533, 2500000], [14, 7.033, 2500000], [15, 7.533, 2500000],
-          [16, 8.033, 2500000], [17, 8.533, 2500000], [18, 9.033, 2500000], [19, 9.533, 2500000], [20, 10.033, 102500000]]
-cf_data = pd.DataFrame(data = cf_raw, columns=['num', 'cash_time', 'cash_amt'])
+# cf_raw = [[1, 0.533, 2500000], [2, 1.033, 2500000], [3, 1.533, 2500000], [4, 2.033, 2500000], [5, 2.533, 2500000],
+#           [6, 3.033, 2500000], [7, 3.533, 2500000], [8, 4.033, 2500000], [9, 4.533, 2500000], [10, 5.033, 2500000],
+#           [11, 5.533, 2500000], [12, 6.033, 2500000], [13, 6.533, 2500000], [14, 7.033, 2500000], [15, 7.533, 2500000],
+#           [16, 8.033, 2500000], [17, 8.533, 2500000], [18, 9.033, 2500000], [19, 9.533, 2500000], [20, 10.033, 102500000]]
+# cf_data = pd.DataFrame(data = cf_raw, columns=['num', 'cash_time', 'cash_amt'])
 
 # spot_curve_raw = [[0.25, 0.449], [0.5, 0.624], [0.75, 0.637], [1, 0.653], [1.5, 0.797], [2, 0.871],
 #                   [2.5, 0.935], [3, 0.984], [4, 1.149], [5, 1.307], [7, 1.393], [10, 1.575], [20, 1.712], [30, 1.695]]
 # spot_yield_data = pd.DataFrame(data = spot_curve_raw, columns=['key_rate', 'yield'])
 logger.debug("key rate duration 산출을 시작합니다.")
-print("채권 현금흐름은 다음과 같습니다. !!!!!!")
-print(cf_data)
 
-print("spot yield는 다음과 같습니다. !!!!!!")
-spot_yield_data = get_spot_yield('20201005', 'C300')
+########################################################################
+# 데이터를 입력합니다.                                                       #
+########################################################################
+# td = '20200904' # key rate duration을 구할 날짜입니다.
+# issue_date = '20190208' # 채권의 발행일
+# due_date  = '20390208' # 채권의 만기일
+# amount = 10000
+# coupon_rate = 0.02191
+# interestpaycalmcnt = 3
+# sectorcode = 'F212'
+#
+# # Bond 객체를 생성합니다.
+# bond1 = Bond(td=td, issue_date=issue_date, due_date=due_date,
+#              amount=amount, coupon_rate=coupon_rate, interestpaycalmcnt=interestpaycalmcnt)
+#
+# bond1_cf = bond1.bond_cf()
+# print("채권 현금흐름은 다음과 같습니다. !!!!!!")
+# print(bond1_cf)
+#
+# print("spot yield는 다음과 같습니다. !!!!!!")
+# spot_yield_data = get_spot_yield(td, sectorcode)
+#
+# print(spot_yield_data)
+#
+# krd = bond1.key_rate_duration(spot_yield_data=spot_yield_data, delta=0.01)
+# print("key rate duration은 다음과 같습니다.\n", krd)
+# print("key rate duration의 합: ", np.sum(krd))
 
-print(spot_yield_data)
+#####################
+# 정답
+# 0.001142808	0.002275107	0.003420771	0.007383728	0.013606139	0.017979911	0.022392036	0.04212059	0.069864591	0.135734599	0.29874631	8.167940176	1.923042661	0	10.70564943
+#################
 
-# Bond 객체를 생성합니다.
-bond1 = Bond(cf_data)
+# 날짜를 입력합니다.
+td = '20200904'
+amount = 10000
+# 해당 날짜의 대상종목을 받아옵니다.
+bond_info = get_bond_info(td)
 
-krd = bond1.key_rate_duration(spot_yield_data=spot_yield_data, interestpaycalmcnt=6, delta=0.01)
-print("key rate duration은 다음과 같습니다.\n", krd)
+# bond_info는 pandas dataframe으로 다음 정보가 있습니다.
+#  securitycode, sectorcode,  issue_date, due_date, coupon_rate, interestpaycalmcnt, dur
+# df는 채권 key rate duration을 저장하는 pandas dataframe입니다.
+df = pd.DataFrame()
+# marks = []
+# range(len(bond_info))
+for i in range(len(bond_info)):
+    info = bond_info.iloc[i]
+    # Bond 객체를 생성합니다.
+    bond = Bond(td=td, issue_date=info['issue_date'], due_date=info['due_date'],
+                amount=amount, coupon_rate=info['coupon_rate'],
+                num_of_interest_payment=info['num_of_interest_payment'],
+                interest_payment_type=info['interest_payment_type'])
+
+    bond_cf = bond.bond_cf()
+    if len(bond_cf) == 0: # 곧 만기가 도래하여 현금흐름이 존재하지 않는 경우
+        krd = [0 for _ in range(14)]
+    else:
+        spot_yield_data = get_spot_yield(td, info['sectorcode'])
+        krd = bond.key_rate_duration(cf_data=bond_cf, spot_yield_data=spot_yield_data, delta=0.01)
+    krd_df = pd.DataFrame(krd).transpose()
+    krd_df.columns = ['M3', 'M6', 'M9', 'Y1', 'Y1.5', 'Y2', 'Y2.5', 'Y3', 'Y4', 'Y5', 'Y7', 'Y10', 'Y20', 'Y30']
+
+    krd_df = krd_df.assign(**{'StdDate': datetime.strptime(td, '%Y%m%d').date(),
+                                'SecurityCode': info['securitycode'],
+                                'KeySum': np.sum(krd, axis = 0)})
+    df = df.append(krd_df)
+    print(i, "번째 key rate duration 산출이 완료되었습니다.")
+print(df)
+
+terminate_time = timeit.default_timer() #종료 시간 체크
+print("%f초 걸렸습니다" %(terminate_time - start_time))
